5cb4e72087f8dbeb0570f2b89a4545cd9a337eb8
Sanghun <po4865@gmail.com>
2023-07-15 16:24:14 +0200

    feat: Update minishell.h - Add lines of include of headers.

--- include/minishell.h
+++ include/minishell.h
@@ -6,15 +6,59 @@
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 15:39:14 by sanghupa          #+#    #+#             */
-/*   Updated: 2023/07/15 15:41:32 by sanghupa         ###   ########.fr       */
+/*   Updated: 2023/07/15 16:03:22 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-// 
-// # include <.h>
+// close read write access dup dup2 execve fork pipe unlink 
+// STDIN_FILENO STDOUT_FILENO getcwd chdir isatty ttyname ttyslot
+# include <unistd.h>
+
+// malloc free exit getenv
+# include <stdlib.h>
+
+// printf perror
+# include <stdio.h>
+
+// opendir readdir closedir
+# include <dirent.h>
+
+// open O_RDONLY O_WRONLY O_RDWR O_CREAT O_TRUNC
+# include <fcntl.h>
+
+// errno EACCES ENOENT
+# include <errno.h>
+
+// strerror
+# include <string.h>
+
+// readline
+# include <readline/readline.h>
+
+// add_history clear_history
+# include <readline/history.h>
+
+// signal kill SIGINT SIGQUIT sigaction sigemptyset sigaddset
+# include <signal.h>
+
+// wait waitpid wait3 wait4
+# include <sys/wait.h>
+
+// stat lstat fstat 
+# include <sys/stat.h>
+
+// ioctl 
+# include <sys/ioctl.h>
+
+// tcsetattr tcgetattr
+# include <termios.h>
+
+// tgetent tgetflag tgetnum tgetstr tgoto tputs
+# include <curses.h>
+# include <term.h>
 
 // # include "libft.h"
 // # include "ft_printf.h"
