e2cbdf06a19689ba22b9a87a1e6f4635d26952ed
Sanghun Park <sanghupa@student.42berlin.de>
2023-07-24 14:32:07 +0200

    feat: Update exit procedure minishell_util.c - Update getcmd() and isexit() funtions to display 'exit\n' string as standard output when encounter the exit situation.   - reference from the 'bash'.

--- src/minishell_util.c
+++ src/minishell_util.c
@@ -6,7 +6,7 @@
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 16:21:57 by sanghupa          #+#    #+#             */
-/*   Updated: 2023/07/24 13:06:33 by sanghupa         ###   ########.fr       */
+/*   Updated: 2023/07/24 14:26:30 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ void	getcmd(char *cmd, size_t len)
 
 	command = readline("> ");
 	if (!command)
+	{
+		ft_putstr_fd("exit\n", STDOUT_FILENO);
 		exit(EXIT_SUCCESS);
+	}
 	len = ft_strlen(command);
 	if (len > MAX_COMMAND_LEN)
 		ft_putstr_fd("warn: command exceed MAX_COMMAND_LEN\n", 2);
@@ -28,5 +31,13 @@ void	getcmd(char *cmd, size_t len)
 
 int	isexit(char *cmd)
 {
-	return (ft_strncmp(cmd, "exit", 4) == 0);
+	int	ret;
+
+	ret = 0;
+	if (ft_strncmp(cmd, "exit", 4) == 0)
+	{
+		ft_putstr_fd("exit\n", STDOUT_FILENO);
+		ret = 1;
+	}
+	return (ret);
 }
