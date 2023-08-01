d65f002c21ec0214451539f1ea09de5a2b51cb23
Mina Kim <mmmiiinn@gmail.com>
2023-08-01 13:17:12 +0200

    ing

--- src/truncate_cmd.c
+++ src/truncate_cmd.c
@@ -6,7 +6,7 @@
 /*   By: minakim <minakim@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 11:59:33 by minakim           #+#    #+#             */
-/*   Updated: 2023/08/01 13:01:18 by minakim          ###   ########.fr       */
+/*   Updated: 2023/08/01 13:06:02 by minakim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,33 +84,17 @@ int	ft_isspace(char c)
 	return (0);
 }
 
-static int	skip_spaces(char *s)
+static int	skip_spaces(char *str)
 {
 	int		i;
 
 	i = -1;
-	while (s[++i])
-		if (!ft_isspace(s[i]))
+	while (str[++i])
+		if (!ft_isspace(str[i]))
 			break ;
 	return (i);
 }
 
-static int	to_next(char *s)
-{
-	int		i;
-	uint8_t	quotes;
-
-	i = -1;
-	quotes = 0;
-	while (s[++i])
-	{
-		if (s[i] == '"')
-			quotes ^= 1;
-		if (!quotes && ft_isspace(s[i]))
-			break ;
-	}
-	return (i);
-}
 
 /// 큰따옴표(")가 있는 str(문자열)를 파라미터(이 함수로 넘어오기전 확인해야 함)를 인수로
 /// 받아 "를 trim하여 malloc의 크기를 바꾼 뒤 return 합니다.
@@ -169,8 +153,27 @@ static char	*trim_quote(char *str)
 }
 
 
+/// 주어진 문자열 str에서 다음 토큰이 시작하는 인덱스를 반환합니다.
+/// 함수는 str의 첫 인덱스(혹은 포인터 시작 위치)부터 시작하여 공백 문자가 아닌 문자를 찾습니다.
+/// 큰따옴표(")에 포함된 공백 문자는 무시합니다.
+/// 큰따옴표가 없거나 짝수개 있을 때(닫혀 있을 때)만 공백 문자로 토큰을 구분합니다.
+static int	to_next(char *str)
+{
+	int		i;
+	uint8_t	quotes;
 
-/// 이 곳에서
+	i = -1;
+	quotes = 0;
+	while (str[++i])
+	{
+		if (str[i] == '"')
+			quotes ^= 1;
+		if (!quotes && ft_isspace(str[i]))
+			break ;
+	}
+	return (i);
+}
+
 char		**tokenise(char *cmd, int size)
 {
 	int		i;
@@ -183,7 +186,12 @@ char		**tokenise(char *cmd, int size)
 	j = 0;
 	while (i < size)
 	{
+		/// j 시작 포인트 (공백 문자를 스킵하고, 공백 문자가 아닌 위치만큼 움직인 뒤,
+		/// 이동한 수만큼 리턴합니다.
 		j += skip_spaces(&cmd[j]);
+		/// len 토큰화 할 범위.
+		/// 큰 따옴표로 닫혀 있다면 큰 따옴표가 있는 범위 만큼(다음 공백문자에서 멈추므로 정확히는 큰따옴표 다음 공백 문자까지)
+		///	아니라면 이어지는 문자열 만큼의 범위를 리턴합니다.
 		len = to_next(&cmd[j]);
 		if (cmd[j] == '"' && cmd[j + len - 1] == '"')
 			token[i] = ft_strndup(&cmd[j + 1], len - 2);
@@ -191,6 +199,8 @@ char		**tokenise(char *cmd, int size)
 			token[i] = ft_strndup(&cmd[j], len);
 		j += len;
 		i++;
+		/// 테스팅이 필요한 부분, "1234""4567" , "1234'1'5678"
+		/// [1234""4567] [1234'1'5678] 처럼 토큰화 된다면 한번 더 파싱하면 되지 않을까 생각하는 중.
 	}
 	return (token);
 }
