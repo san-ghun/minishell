92017c9e7d70900a8c40c41c89eca6de2dc1abfe
Sanghun <po4865@gmail.com>
2023-08-25 23:25:58 +0200

    feat: Add new utility functions ft_isspace() and ft_strndup()

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 15:39:14 by sanghupa          #+#    #+#             */
-/*   Updated: 2023/08/24 11:56:45 by sanghupa         ###   ########.fr       */
+/*   Updated: 2023/08/25 23:24:40 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,10 +99,12 @@ size_t	ft_strspn(const char *str, const char *delim);
 char	*ft_strtok(char *str, const char *delim);
 size_t	ft_strcspn(const char *str, const char *delim);
 char	*ft_strncpy(char *dest, const char *src, size_t size);
+char	*ft_strndup(const char *src, size_t len);
 void	*ft_memalloc(size_t size);
 char	*ft_strcdup(const char *src, char c);
 int		ft_strequ(char const *s1, char const *s2);
 int		ft_strcmp(char const *s1, char const *s2);
+int		ft_isspace(char c);
 
 // struct t_sent
 /// @param token Save the cmd, its arguments, and enough to run at once, 
---
+++ src/util/ft_strndup.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/25 23:13:26 by sanghupa          #+#    #+#             */
+/*   Updated: 2023/08/25 23:23:14 by sanghupa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/**
+ * @brief Duplicates a substring from `src` up to `len` characters.
+ * If a pointer is passed, `src` starts from the position of the pointer.
+ * @param src The source string.
+ * @param len The length of characters to copy.
+ * @return A newly allocated substring, or NULL on allocation failure.
+ */
+char	*ft_strndup(const char *src, size_t len)
+{
+	char	*new;
+
+	new = (char *)ft_memalloc(len + 1);
+	if (!new)
+		return (NULL);
+	return (ft_strlcpy(new, src, len));
+}
