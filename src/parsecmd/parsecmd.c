e8bfa61d197c87ab4bcce27e26439a6b9da16c45
Sanghun <po4865@gmail.com>
2023-08-30 17:07:02 +0200

    feat: Rebuild parsecmd() to convert input to t_sent and add to deque src/parsecmd/parsecmd.c - Add three fucntions cmdtosent(), get_punit(), and split_cmd() to process read line to t_sent.
    src/t_sent/sent_create.c
    - Remove functions those used for processinig cmd for t_sent.
    - Edit sent_new() for the change.

--- src/parsecmd/parsecmd.c
+++ src/parsecmd/parsecmd.c
@@ -6,7 +6,7 @@
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 12:38:54 by sanghupa          #+#    #+#             */
-/*   Updated: 2023/08/25 23:44:16 by sanghupa         ###   ########.fr       */
+/*   Updated: 2023/08/30 17:02:00 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,61 @@ int	check_quotes(char *cmd, int index, int status)
 	return (0);
 }
 
+static char	*get_punit(char *margv[], int i, int select)
+{
+	char	punit[DATA_SIZE];
+
+	ft_strlcpy(punit, "", 2);
+	while (select < i)
+	{
+		ft_strlcat(punit, margv[select], \
+					ft_strlen(punit) + ft_strlen(margv[select]) + 1);
+		select++;
+		if (select < i)
+			ft_strlcat(punit, " ", ft_strlen(punit) + 1 + 1);
+	}
+	return (ft_strdup(punit));
+}
+
+static int	split_cmd(t_sent *node, char *margv[], int i, int select)
+{
+	int	tmp;
+
+	tmp = 0;
+	node->tokens_len = i - select;
+	node->tokens = (char **)ft_memalloc(sizeof(char *) * (i - select) + 1);
+	while (select < i)
+		node->tokens[tmp++] = ft_strdup(margv[select++]);
+	return (select);
+}
+
+static int	cmdtosent(int margc, char *margv[], t_deque *deque)
+{
+	int		i;
+	int		select;
+	t_sent	*node;
+
+	i = -1;
+	select = 0;
+	while (++i < margc)
+	{
+		if (ft_strequ(margv[i], "|"))
+		{
+			node = sent_new(get_punit(margv, i, select), 0, 1);
+			select = split_cmd(node, margv, i, select) + 1;
+			deque_push_front(deque, node);
+			i++;
+		}
+		else if (margc == i + 1)
+		{
+			node = sent_new(get_punit(margv, i + 1, select), 0, 0);
+			select = split_cmd(node, margv, i + 1, select);
+			deque_push_front(deque, node);
+		}
+	}
+	return (i);
+}
+
 int	parsecmd(char *cmd, t_deque *deque)
 {
 	int		i;
@@ -52,16 +107,12 @@ int	parsecmd(char *cmd, t_deque *deque)
 		ft_putstr_fd("error: Invalid quotation\n", 2);
 		return (-1);
 	}
-
 	// Expand $dollar
-
 	// Expand ~tilde
 
 	margc = get_margc(cmd);
 	margv = get_margv(cmd, margc);
-
-	// Create a new sent
-	// Add the new sent to the deque
+	cmdtosent(margc, margv, deque);
 
 	ft_printf("margc: %d\n", margc);
 	i = 0;
--- src/t_sent/sent_create.c
+++ src/t_sent/sent_create.c
@@ -6,72 +6,13 @@
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 16:07:12 by sanghupa          #+#    #+#             */
-/*   Updated: 2023/08/24 16:50:10 by sanghupa         ###   ########.fr       */
+/*   Updated: 2023/08/30 16:47:24 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 t_sent	*sent_new(char *p_unit, int is_redir, int is_pipe);
-void	sent_addfront(t_sent *sent[], t_sent *new);
-void	sent_addback(t_sent *sent[], t_sent *new);
-
-static int	get_tokens_len(char *cmd)
-{
-	int		i;
-	char	p[2];
-	char	*tmp;
-	char	*token;
-
-	i = 0;
-	tmp = ft_strdup(cmd);
-	ft_strlcpy(p, (tmp + ft_strcspn(tmp, "\'\" ")), 2);
-	token = ft_strtok(tmp, p);
-	while ((token != NULL) && (i < MAX_TOKENS))
-	{
-		i++;
-		tmp += ft_strspn(tmp, "\'\" ");
-		ft_strlcpy(p, (tmp + ft_strcspn(tmp, "\'\" ")), 2);
-		token = ft_strtok(NULL, p);
-	}
-	// if (tmp != NULL)
-	free(tmp);
-	return (i);
-}
-
-static int	tokenize_intochunk(char *cmd, char *tokens[], int tokens_len)
-{
-	int		i;
-	char	p[2];
-	char	*token;
-
-	i = 0;
-	ft_strlcpy(p, (cmd + ft_strcspn(cmd, "\'\" ")), 2);
-	token = ft_strtok(cmd, p);
-	while ((i < tokens_len) && (token != NULL) && (i < MAX_TOKENS))
-	{
-		tokens[i++] = ft_strtrim(token, " ");
-		cmd += ft_strspn(cmd, "\'\" ");
-		ft_strlcpy(p, (cmd + ft_strcspn(cmd, "\'\" ")), 2);
-		token = ft_strtok(NULL, p);
-	}
-	return (i);
-}
-
-static char	**tokenize_process(char *p_unit, int tokens_len)
-{
-	// char	tmp[MAX_COMMAND_LEN / 100];
-	char	*tmp;
-	char	**tokens;
-
-	// ft_strlcpy(tmp, "", 2);
-	// ft_strlcpy(tmp, p_unit, ft_strlen(p_unit) + 2);
-	tmp = ft_strdup(p_unit);
-	tokens = (char **)ft_memalloc(sizeof(char *) * tokens_len + 1);
-	tokenize_intochunk(tmp, tokens, tokens_len);
-	free(tmp);
-	return (tokens);
-}
 
 t_sent	*sent_new(char *p_unit, int is_redir, int is_pipe)
 {
@@ -80,9 +21,9 @@ t_sent	*sent_new(char *p_unit, int is_redir, int is_pipe)
 	this = (t_sent *)malloc(sizeof(t_sent));
 	if (!this)
 		return (NULL);
-	this->p_unit = ft_strdup(p_unit);
-	this->tokens_len = get_tokens_len(p_unit);
-	this->tokens = tokenize_process(p_unit, this->tokens_len);
+	this->p_unit = p_unit;
+	this->tokens_len = 0;
+	this->tokens = NULL;
 	this->is_redir = is_redir;
 	this->is_pipe = is_pipe;
 	this->prev = NULL;
