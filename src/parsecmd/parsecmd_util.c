b68a17a6850cba4b5b45f952be5a4bd58fd2b402
Sanghun <po4865@gmail.com>
2023-09-04 13:07:27 +0200

    feat: Update expanding of dallar and tilde for parsecmd include/minishell.h - Move parsecmd() relate functions to bottom of the file, because the functions need t_elst which are env related structs.
    src/minishell.c
    - 31: Edit cmd initializing line to fix bug and make sure the string value init with zero.
    - 100: Remove line of code in non-usage.
    - 107: Update a param of parsecmd() for the edit of the function.
    
    src/parsecmd/parsecmd.c
    - 99: Update parsecmd() to take t_elst as an arg to process env vals.
    - 110: Aggregate the expand functions for dallar and tilde sign into one function, call expand_cmd().
    
    src/parsecmd/parsecmd_util.c
    - 35-36: In append_env(), add if-statement for expanding tilde character to process tilde during parsing.
    - 58-60: In expand_cmd(), add if-statements to process dallar and tilde characters.

--- src/parsecmd/parsecmd_util.c
+++ src/parsecmd/parsecmd_util.c
@@ -6,37 +6,13 @@
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 13:03:00 by sanghupa          #+#    #+#             */
-/*   Updated: 2023/09/02 12:35:19 by minakim          ###   ########.fr       */
+/*   Updated: 2023/09/04 12:56:59 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	append_env(char *str, char *cmd, char *envp[])
-{
-	int		i;
-	int		envpi;
-
-	i = 1;
-	while (ft_isalnum(cmd[i]))
-		i++;
-	i--;
-	envpi = 0;
-	while (envp[envpi])
-	{
-		if ((ft_strncmp(envp[envpi], &cmd[1], i) == 0) && \
-			(envp[envpi][i] == '='))
-		{
-			ft_strlcat(str, &envp[envpi][i + 1], \
-				ft_strlen(str) + ft_strlen(&envp[envpi][i + 1]) + 1);
-			break ;
-		}
-		envpi++;
-	}
-	return (i);
-}
-
-static int	append_env_edit(char *str, char *cmd, t_elst *lst)
+static int	append_env(char *str, char *cmd, t_elst *lst)
 {
 	int		i;
 	t_env	*node;
@@ -50,22 +26,25 @@ static int	append_env_edit(char *str, char *cmd, t_elst *lst)
 	{
 		if (ft_strnequ(node->key, &cmd[1], i))
 		{
-			ft_strlcat(str, node->value, ft_strlen(str) + ft_strlen(node->value) + 1);
+			ft_strlcat(str, node->value, \
+						ft_strlen(str) + ft_strlen(node->value) + 1);
 			break ;
 		}
 		node = node->next;
 	}
+	if (cmd[0] == '~')
+		i = 0;
 	return (i);
 }
 
-void	expand_dallar(char *cmd, char *envp[])
+void	expand_cmd(char *cmd, t_elst *elst)
 {
 	char	str[MAX_COMMAND_LEN];
 	int		i;
 	uint8_t	quote_s;
 	uint8_t	quote_d;
 
-	ft_strlcpy(str, "", 2);
+	ft_bzero(&str[0], MAX_COMMAND_LEN);
 	i = 0;
 	quote_s = 0;
 	quote_d = 0;
@@ -76,61 +55,9 @@ void	expand_dallar(char *cmd, char *envp[])
 		else if (cmd[i] == '\"' && quote_s != 1)
 			quote_d ^= 1;
 		if (cmd[i] == '$' && quote_s != 1)
-			i += append_env(&str[0], &cmd[i], envp);
-		else
-			ft_strlcat(&str[0], &cmd[i], ft_strlen(str) + 2);
-		i++;
-	}
-	ft_strlcpy(cmd, str, ft_strlen(str) + 1);
-	return ;
-}
-
-void	expand_tilde(char *cmd, char *envp[])
-{
-	char	str[MAX_COMMAND_LEN];
-	int		i;
-	uint8_t	quote_s;
-	uint8_t	quote_d;
-
-	ft_strlcpy(str, "", 2);
-	i = 0;
-	quote_s = 0;
-	quote_d = 0;
-	while (cmd[i] != '\0')
-	{
-		if (cmd[i] == '\'' && quote_d != 1)
-			quote_s ^= 1;
-		else if (cmd[i] == '\"' && quote_s != 1)
-			quote_d ^= 1;
-		if (cmd[i] == '~' && quote_s != 1)
-			i += append_env(&str[0], "~HOME", envp);
-		else
-			ft_strlcat(&str[0], &cmd[i], ft_strlen(str) + 2);
-		i++;
-	}
-	ft_strlcpy(cmd, str, ft_strlen(str) + 1);
-	return ;
-}
-
-void	expand_tilde_edit(char *cmd, t_elst *lst)
-{
-	char	str[MAX_COMMAND_LEN];
-	int		i;
-	uint8_t	quote_s;
-	uint8_t	quote_d;
-
-	ft_strlcpy(str, "", 2);
-	i = 0;
-	quote_s = 0;
-	quote_d = 0;
-	while (cmd[i] != '\0')
-	{
-		if (cmd[i] == '\'' && quote_d != 1)
-			quote_s ^= 1;
-		else if (cmd[i] == '\"' && quote_s != 1)
-			quote_d ^= 1;
-		if (cmd[i] == '~' && quote_s != 1)
-			i += append_env_edit(&str[0], "~HOME", lst);
+			i += append_env(&str[0], &cmd[i], elst);
+		else if (cmd[i] == '~' && quote_s != 1)
+			i += append_env(&str[0], "~HOME", elst);
 		else
 			ft_strlcat(&str[0], &cmd[i], ft_strlen(str) + 2);
 		i++;
