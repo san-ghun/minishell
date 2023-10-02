0805df2a98046178991dea7aa38a8d211bee5696
Mina Kim <minakim.dev@gamil.com>
2023-10-02 22:42:50 +0200

    fix : remove unused var

--- src/t_sent/sent_create.c
+++ src/t_sent/sent_create.c
@@ -6,7 +6,7 @@
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 16:07:12 by sanghupa          #+#    #+#             */
-/*   Updated: 2023/10/01 15:43:51 by minakim          ###   ########.fr       */
+/*   Updated: 2023/10/02 22:41:36 by minakim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ t_sent	*sent_new(char *p_unit, int input_flag, int output_flag)
 	this->tokens = NULL;
 	this->input_flag = input_flag;
 	this->output_flag = output_flag;
-	this->exit_status = FALSE;
 	this->input_argv = NULL;
 	this->output_argv = NULL;
 	this->prev = NULL;
