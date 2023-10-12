d6cf0b3764b7487d9231a086ee20dff508e31eda
Sanghun <po4865@gmail.com>
2023-10-12 12:45:09 +0200

    feat: Update tokenizer to parse "abc"abd kinds

--- src/parsecmd/parsecmd_tokenize.c
+++ src/parsecmd/parsecmd_tokenize.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parse_quotes.c                                     :+:      :+:    :+:   */
+/*   parsecmd_tokenize.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 23:43:14 by sanghupa          #+#    #+#             */
-/*   Updated: 2023/08/25 23:45:51 by sanghupa         ###   ########.fr       */
+/*   Updated: 2023/10/12 12:42:50 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,14 @@ int	get_margc(char *cmd)
 	quote_d = 0;
 	while (cmd[++i])
 	{
-		if (cmd[i] == '\'' && quote_d != 1)
-			quote_s ^= 1;
-		else if (cmd[i] == '\"' && quote_s != 1)
-			quote_d ^= 1;
-		if (!quote_s && !quote_d && !ft_isspace(cmd[i]) && \
-			(ft_isspace(cmd[i + 1]) || !(cmd[i + 1])))
+		// if (cmd[i] == '\'' && quote_d != 1)
+		// 	quote_s ^= 1;
+		// else if (cmd[i] == '\"' && quote_s != 1)
+		// 	quote_d ^= 1;
+		// if (!quote_s && !quote_d && !ft_isspace(cmd[i]) && \
+		// 	(ft_isspace(cmd[i + 1]) || !(cmd[i + 1])))
+		// 	cnt++;
+		if (!ft_isspace(cmd[i]) && (ft_isspace(cmd[i + 1]) || !(cmd[i + 1])))
 			cnt++;
 	}
 	return (cnt);
@@ -61,14 +63,35 @@ static int	get_nexti(char *s)
 	quote_d = 0;
 	while (s[++i])
 	{
-		if (s[i] == '\'' && quote_d != 1)
-			quote_s ^= 1;
-		else if (s[i] == '\"' && quote_s != 1)
-			quote_d ^= 1;
-		if (!quote_s && !quote_d && ft_isspace(s[i]))
+		if (ft_isspace(s[i]))
 			break ;
 	}
 	return (i);
+}
+
+char	*ms_strndup(const char *src, size_t len)
+{
+	int		i;
+	int		j;
+	char	*new;
+
+	new = (char *)ft_memalloc(len + 1);
+	if (!new)
+		return (NULL);
+	i = -1;
+	j = -1;
+	while (src[++i] != '\0' && i < len)
+	{
+		if ((src[i] == '\"') || (src[i] == '\''))
+			continue ;
+		new[++j] = src[i];
+	}
+	while (i < len)
+	{
+		new[i] = '\0';
+		i++;
+	}
+	return (new);
 }
 
 char	**get_margv(char *cmd, int margc)
@@ -81,16 +104,12 @@ char	**get_margv(char *cmd, int margc)
 	margv = (char **)ft_memalloc(sizeof(char *) * (margc + 1));
 	i = -1;
 	select = 0;
+	ft_printf("margc: %d\n", margc);
 	while (++i < margc)
 	{
 		select += skip_spaces(&cmd[select]);
 		nexti = get_nexti(&cmd[select]);
-		if (cmd[select] == '\"')
-			margv[i] = ft_strcdup(&cmd[select + 1], '\"');
-		else if (cmd[select] == '\'')
-			margv[i] = ft_strcdup(&cmd[select + 1], '\'');
-		else
-			margv[i] = ft_strndup(&cmd[select], nexti);
+		margv[i] = ms_strndup(&cmd[select], nexti);
 		select += nexti;
 	}
 	return (margv);
