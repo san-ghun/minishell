636a746eb2b2fd36e0c6fc62715f5e5974d23bc4
minakim <minakim@student.42berlin.de>
2023-11-17 17:04:06 +0100

    fix : `dhdhdhd |||||||`

--- src/executecmd/executecmd_check.c
+++ src/executecmd/executecmd_check.c
@@ -6,7 +6,7 @@
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 16:38:30 by sanghupa          #+#    #+#             */
-/*   Updated: 2023/11/17 15:49:11 by minakim          ###   ########.fr       */
+/*   Updated: 2023/11/17 17:02:49 by minakim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,29 @@ int	check_pid(pid_t pid)
 	return (0);
 }
 
+int	check_all_cmd(t_sent *cmd)
+{
+	t_sent	*tmp;
+
+	tmp = cmd;
+	while (cmd->next != NULL)
+	{
+		if (cmd->output_flag == PIPE_FLAG)
+		{
+			if (ft_strequ(tmp->tokens[0], "|") \
+			|| (ft_strequ(tmp->next->tokens[0], "|")))
+			{
+				cmd->output_flag = STDERR_FILENO;
+				cmd->output_argv = ft_strdup(ONLY_PIPES);
+				ft_putstr_fd(cmd->output_argv, 2);
+				return (-1);
+			}
+		}
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
 int	is_only_pipe(t_sent *cmd, int total_cmd_count)
 {
 	if (cmd->tokens[0] == NULL && cmd->output_flag == PIPE_FLAG)
@@ -57,7 +80,7 @@ int	is_only_pipe(t_sent *cmd, int total_cmd_count)
 			return (-1);
 		}
 	}
-	return (0);
+	return (check_all_cmd(cmd));
 }
 
 int	check_dir(char *cmd, char **path)
